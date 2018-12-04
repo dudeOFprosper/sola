69d2ca49be8ccc70cd014685512e073ed2fe8680
Bill Pond <william.e.pond@gmail.com>
2018-12-03 23:12:48 -0500

    Updates

---
+++ precache-manifest.e4915efdfb47549d55b57abdb3fe0f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ff10989b6a72f13e88a994347bec05",
+    "url": "/sola/static/media/sola.d5ff1098.gif"
+  },
+  {
+    "revision": "242bd58cc6bd5cfbcb56",
+    "url": "/sola/static/js/runtime~main.242bd58c.js"
+  },
+  {
+    "revision": "b6d8bd789326c17554a1",
+    "url": "/sola/static/js/main.b6d8bd78.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/sola/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "b6d8bd789326c17554a1",
+    "url": "/sola/static/css/main.78146a7b.chunk.css"
+  },
+  {
+    "revision": "70c73d5e2b450607dab0bb3e4307291a",
+    "url": "/sola/index.html"
+  }
+];
