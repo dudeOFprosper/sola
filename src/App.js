4165bb6bca49ed9784e1b9d43b094e4d1e19a689
Bill Pond <william.e.pond@gmail.com>
2018-12-03 23:18:55 -0500

    repeat it!

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
     );
